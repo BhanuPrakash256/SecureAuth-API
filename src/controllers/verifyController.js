7105728c55afd3804924eacc63914c697ec7581e
BhanuPrakash256 <bhanuprakash256sp@gmail.com>
2024-08-07 21:44:11 +0530

    Update Verification status and Token issuance

---
+++ src/controllers/verifyController.js
@@ -0,0 +1,82 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+// Function to verify email
+exports.verifyEmail = async (req, res) => {
+    try {
+      const { username, code } = req.body;
+      
+      // Find the user by the email verification token
+      const user = await User.findOne({ username });
+  
+      if (user.emailVerificationCode !== code) {
+          return res.status(404).json({ message: 'Invalid verification code' });
+        }
+      // Update user's email verification status
+      user.emailVerified = true;
+      user.emailVerificationCode = undefined; // Remove the token after verification
+      await user.save();
+  
+      res.status(200).json({ message: 'Email verified successfully' });
+    } catch (error) {
+      console.error('Error verifying email:', error);
+      res.status(500).json({ message: 'Error verifying email' });
+    }
+  };
+  
+  // Controller function to verify phone number
+  exports.verifyPhoneNumber = async (req, res) => {
+    try {
+      const { username, code } = req.body;
+      const user = await User.findOne({ username, phoneVerificationCode: code });
+      
+      if (!user) {
+          console.log(user.phoneVerificationCode, code);
+          return res.status(400).json({ message: 'Invalid code' });
+      }
+      
+      user.phoneVerified = true;
+      user.phoneVerificationCode = undefined;
+      
+      await user.save();
+      res.status(200).json({ message: 'Phone number verified successfully' });
+    
+    } catch (error) {
+      res.status(500).json({ message: 'Error verifying phone number', error });
+    }
+  };
+
+
+  exports.updateVerificationStatus = async (req, res) => {
+    try {
+      const { username } = req.params;
+      const user = await User.findOne({ username });
+
+  
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      if (user.emailVerified && user.phoneVerified) {
+        user.verificationStatus = 'verified';
+        await user.save();
+        
+        const token = jwt.sign(
+        {
+          UserId: user._id, 
+          username: user.username
+        }, 
+          config.jwtSecret,
+          {expiresIn: '23h'}
+    );
+  
+        return res.status(200).json({ message: 'User verified successfully', token });
+      }
+  
+      res.status(400).json({ message: 'Email and phone number verification required' });
+    } catch (error) {
+      res.status(500).json({ message: 'Error updating verification status', error });
+      console.log(error);
+    }
+  };
